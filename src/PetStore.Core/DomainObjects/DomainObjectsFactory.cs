614247aa4f7be0fa7dc9a35059d5974d9dec3a9f
Andre Azevedo <andre.azevedo@gmail.com>
2011-08-08 19:28:18 -0300

    Entity creation infrastructure. related #1

---
+++ src/PetStore.Core/DomainObjects/DomainObjectsFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetStore.Core.DomainObjects
+{
+    /// <summary>
+    /// Entity creator.
+    /// </summary>
+    public static class DomainObjectsFactory
+    {
+    }
+}
