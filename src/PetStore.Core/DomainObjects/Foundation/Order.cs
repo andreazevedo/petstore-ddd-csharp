2b9223810747fbfc54d8879cb30401e9cbba36ba
Leonardo Campos <leonardoscampos@gmail.com>
2011-08-17 03:30:14 -0300

    Adding Order and OrderItem entities. related #2

---
+++ src/PetStore.Core/DomainObjects/Foundation/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using PetStore.Core.DomainObjects.Payment;
+using PetStore.Core.DomainObjects.Security;
+
+namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Order entity
+    /// </summary>
+    public class Order
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Order ID
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Order user
+        /// </summary>
+        public virtual User User { get; set;}
+
+        /// <summary>
+        /// Credit card used to pay the order
+        /// </summary>
+        public virtual CreditCard CreditCard { get; set; }
+
+        /// <summary>
+        /// Order billing address
+        /// </summary>
+        public virtual Address BillingAddress { get; set; }
+
+        /// <summary>
+        /// Order shipping address
+        /// </summary>
+        public virtual Address ShippingAddress { get; set; }
+
+        /// <summary>
+        /// Order items
+        /// </summary>
+        public virtual IList<OrderItem> Items { get; set; }
+
+        /// <summary>
+        /// Order total
+        /// </summary>
+        public virtual decimal Total { get; set; }
+
+
+        /// <summary>
+        /// Credit card authorization number
+        /// TODO - It must be on CreditCardPayment entity
+        /// </summary>
+        public virtual int? AuthorizationNumber { get; set; }
+
+        /// <summary>
+        /// Order creation date
+        /// </summary>
+        public virtual DateTime CreatedAt { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/PetStore.Core/DomainObjects/Foundation/OrderItem.cs
@@ -0,0 +1,48 @@
+﻿namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Order item entity
+    /// </summary>
+    public class OrderItem
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Order item ID
+        /// </summary>
+        public virtual int Id { get; set; }
+        
+        /// <summary>
+        /// Order that contains this item
+        /// </summary>
+        public virtual Order Order { get; set; }
+
+        /// <summary>
+        /// Product
+        /// </summary>
+        public virtual Product Product { get; set; }
+
+        /// <summary>
+        /// Product quantity
+        /// </summary>
+        public virtual int Quantity { get; set; }
+
+        /// <summary>
+        /// Product price
+        /// </summary>
+        public virtual decimal Price { get; set; }
+
+        /// <summary>
+        /// Order item subtotal
+        /// </summary>
+        public virtual decimal SubTotal
+        {
+            get
+            {
+                return Quantity*Price;
+            }
+        }
+
+        #endregion
+    }
+}
