37807258099659b3e6cf7c58de1c7652eef1142f
Leonardo Campos <leonardoscampos@gmail.com>
2011-08-17 00:13:37 -0300

    Adding product entity. related #2

---
+++ src/PetStore.Core/DomainObjects/Foundation/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Entidade que representa um produto
+    /// </summary>
+    public class Product
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Id único do produto
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Categoria do produto
+        /// </summary>
+        public Category Category { get; set; }
+
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Descrição do produto
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Caminho da imagem do produto
+        /// </summary>
+        public string ImagePath { get; set; }
+
+        #endregion
+    }
+}
