c81756794d65312cbbaf8c9af6118c256a459ff2
Leonardo Campos <leonardoscampos@gmail.com>
2011-08-17 00:05:53 -0300

    Adding product category entity. related #2

---
+++ src/PetStore.Core/DomainObjects/Foundation/Category.cs
@@ -0,0 +1,27 @@
+﻿namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Entidade que representa uma categoria de produtos
+    /// </summary>
+    public class Category
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Id da categoria
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome da Categoria
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Descrição da categoria
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion
+    }
+}
