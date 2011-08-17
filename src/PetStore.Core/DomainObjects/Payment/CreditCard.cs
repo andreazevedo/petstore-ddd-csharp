9cf68d546a9849570d094aab69125ece05c1cddf
Leonardo Campos <leonardoscampos@gmail.com>
2011-08-17 03:17:25 -0300

    Adding CreditCardAssociation and CreditCard entities. related #2

---
+++ src/PetStore.Core/DomainObjects/Payment/CreditCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PetStore.Core.DomainObjects.Payment
+{
+    public class CreditCard
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Credit card number
+        /// </summary>
+        public virtual string Number { get; set; }
+
+        /// <summary>
+        /// Credit card expiration date
+        /// </summary>
+        public virtual DateTime ExpirationDate { get; set; }
+
+        /// <summary>
+        /// Credit card association (Visa, Mastercard, etc.)
+        /// </summary>
+        public virtual CreditCardAssociation Association { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/PetStore.Core/DomainObjects/Payment/CreditCardAssociation.cs
@@ -0,0 +1,22 @@
+﻿namespace PetStore.Core.DomainObjects.Payment
+{
+    /// <summary>
+    /// Credit card association entity
+    /// </summary>
+    public class CreditCardAssociation
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Credit card association ID
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Credit card association name (Visa, Mastercard, American Express, etc)
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        #endregion
+    }
+}
