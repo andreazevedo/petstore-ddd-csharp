85f2d1f8a9fd6cc983101619f591f50f039bcde8
Leonardo Campos <leonardoscampos@gmail.com>
2011-08-17 00:44:11 -0300

    Adding country entity. related #2

---
+++ src/PetStore.Core/DomainObjects/Foundation/Country.cs
@@ -0,0 +1,27 @@
+﻿namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Country entity
+    /// </summary>
+    public class Country
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Country ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Country name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Country code (acronym) defined on (ISO 3166-1 alfa-2)
+        /// </summary>
+        public string Code { get; set; }
+
+        #endregion
+    }
+}
