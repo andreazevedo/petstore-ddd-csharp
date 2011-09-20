e558a6300af52b82748b0d7da906f115a3bc45c0
Leonardo Campos <leonardoscampos@gmail.com>
2011-09-20 00:09:56 -0300

    Added PhoneNumberType list

---
+++ src/PetStore.Core/DomainObjects/Foundation/PhoneNumberType.cs
@@ -0,0 +1,14 @@
+﻿namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Phone Number Types
+    /// </summary>
+    public enum PhoneNumberType
+    {
+        Home = 1,
+        Mobile = 2,
+        Office = 3,
+        Fax = 4,
+        Other = 5
+    }
+}
