bcae50047bc847680000c22344f27a3019dc7847
Leonardo Campos <leonardoscampos@gmail.com>
2011-09-20 00:29:49 -0300

    Adding PhoneNumber entity. related #2

---
+++ src/PetStore.Core/DomainObjects/Foundation/PhoneNumber.cs
@@ -0,0 +1,42 @@
+﻿namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Phone Number entity
+    /// </summary>
+    public class PhoneNumber
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Phone number Id
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Phone number type
+        /// </summary>
+        public virtual PhoneNumberType Type { get; set; }
+
+        /// <summary>
+        /// Phone number country code
+        /// </summary>
+        public virtual string CountryCode { get; set; }
+
+        /// <summary>
+        /// Phone number area code
+        /// </summary>
+        public virtual string AreaCode { get; set; }
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        public virtual string Number { get; set; }
+
+        /// <summary>
+        /// Phone number contact name
+        /// </summary>
+        public virtual string ContactName { get; set; }
+
+        #endregion
+    }
+}
