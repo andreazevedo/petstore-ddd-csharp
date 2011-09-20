3039a7ab20d88878ba3e68e69e89e75aab9d9dfc
Leonardo Campos <leonardoscampos@gmail.com>
2011-09-20 00:42:50 -0300

    Adding Supplier entity. related #2

---
+++ src/PetStore.Core/DomainObjects/Foundation/Supplier.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace PetStore.Core.DomainObjects.Foundation
+{
+    /// <summary>
+    /// Supplier entity
+    /// </summary>
+    public class Supplier
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Supplier Id
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Supplier Name
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// Supplier Address
+        /// </summary>
+        public virtual Address Address { get; set; }
+
+        /// <summary>
+        /// Supplier phone numbers
+        /// </summary>
+        public virtual IList<PhoneNumber> PhoneNumbers { get; set; }
+
+        /// <summary>
+        /// Indicates if the supplier is active
+        /// </summary>
+        public virtual bool IsActive { get; set; }
+
+        #endregion
+    }
+}
